31c95fe4662ad776351e031a97d6c8c8e57c73f2
rajneeshgithub73 <rajneeshrj73@gmail.com>
2023-12-17 01:38:56 +0530

    user controller

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import express, { urlencoded } from "express";
+import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
@@ -28,7 +28,10 @@ app.use(express.static("public"));
 
 app.use(cookieParser());
 
+//routes import
+import userRouter from "./routes/user.routes.js"
 
-
+//routes declaration
+app.use("/api/v1/users", userRouter)
 
 export { app };
